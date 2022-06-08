f9d9125b2e77cc5375f134d382e588ce89e86452
Okera Beckford <obeckford@mitek.co.uk>
2022-06-08 12:31:27 +0100

    change url tree method

--- exercises/src/app/guards/auth.guard.ts
+++ exercises/src/app/guards/auth.guard.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
     : Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (!this.authService.isLoggedIn())
-      return this.route.createUrlTree(['']);
+      return this.route.parseUrl('');
 
     return true;
   }
