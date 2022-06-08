6a78d4667b0c7b31aeb16b9702703e028579d028
MiTek-Kova <tshabudin@mii.com>
2022-06-08 16:38:13 +0300

    Minor correction to path

--- exercises/src/app/app-routing.module.ts
+++ exercises/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'user-list', component: AdminUserlistComponent },
   { path: 'user-details/:userId', component: UserDetailsComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full '},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: ErrorComponent }
 ]
 
